ef2b44b64d37ea8f1d0edbf10adb0044ee23ae57
mikejguo290 <mikejguo290@gmail.com>
2021-07-14 11:56:38 +0100

    define renderAction to conditionally render buttons with different content. use it in Track.

--- src/Track/Track.js
+++ src/Track/Track.js
@@ -1,5 +1,18 @@
 import './Track.css';
 
+// not good practice to define functions inside other functions. as these are redefined each time the container function is called
+// not possible to have methods in stateless functional components.
+// so define a class method outside the function and call it with functionName.bind(null, parameters) in Track.
+
+const renderAction = (isRemoval)=>{
+    // displays a <button> element with - as its content if the isRemoval property is true, and a + <button> element if the isRemoval property is false
+    if (isRemoval){
+        return (<button className="Track-action">-</button>);
+    }else{
+        return (<button className="Track-action">+</button>);
+    };    
+}; 
+
 function Track (){
     return (
         <div className="Track">
@@ -7,7 +20,8 @@ function Track (){
                 <h3>{/* track name will go here */}</h3>
                 <p>{/* <-- track artist will go here--> | <!-- track album will go here --> */}</p>
             </div>
-            <button className="Track-action">{/* <!-- + or - will go here --> */}</button>
+            {/* button <!-- + or - will go here --> */}
+            {renderAction.bind(null, isRemoval)}
         </div>
     );
 }

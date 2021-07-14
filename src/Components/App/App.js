84f987020635fc038660ffbe6852d8eb7e65f11a
mikejguo290 <mikejguo290@gmail.com>
2021-07-14 13:11:42 +0100

    pull in props from React.component class, mock 3 tracks for searchResults state.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,8 +2,14 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import useState from 'react';
 
-function App() {
+function App(props) {
+  const [searchResults, setSearchResults] = useState([
+    {name:'I feel it coming', artist:'The Weeknd', album:'Starboy'},
+    {name:'Physical', artist:'Dua Lipa', album:'Club Future Nostalgia'},
+    {name:'7 rings', artist:'Ariana Grande', album:'thank u, next'}
+  ]);
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>

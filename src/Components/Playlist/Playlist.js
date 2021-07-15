769ba4f3ffdbd83e4325d7868d24e47aa54eb4a9
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 13:23:11 +0100

    create handleNameChange event handler and use it with input element's onChange.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,9 +2,16 @@ import './Playlist.css';
 import TrackList from '../TrackList/TrackList';
 
 function Playlist(props){
+    
+    const handleNameChange = (event) =>{
+        // on every input change, call updatePlaylistName passed down from App to update state playlistName.
+        const name = event.target.value;
+        props.onNameChange(name);
+    };
+
     return (
         <div className="Playlist">
-            <input defaultValue="New Playlist" value={props.playlistName} />
+            <input defaultValue="New Playlist" value={props.playlistName} onChange={handleNameChange}/>
             {/* Add a TrackList component */}
             <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>

73bf8b1e19e7fca752e6b93023e5f4af5d8db1fb
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 12:51:11 +0100

    set playlist's input default value and set its value to props.playlistName.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,7 +4,7 @@ import TrackList from '../TrackList/TrackList';
 function Playlist(props){
     return (
         <div className="Playlist">
-            <input defaultValue={props.playlistName} />
+            <input defaultValue="New Playlist" value={props.playlistName} />
             {/* Add a TrackList component */}
             <TrackList tracks={props.playlistTracks} onRemove={props.onRemove} isRemoval={true} />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>

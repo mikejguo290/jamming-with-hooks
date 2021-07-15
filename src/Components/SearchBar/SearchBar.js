892073421240354b2c7a81399c29ef88775669b4
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 15:34:28 +0100

    add onChange event handler to update SearchBar's searchTerm state.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,18 +7,25 @@ function SearchBar(props){
     and yet it isn't tracked at App level? contravenes the 4th principle of thinking in react.  
     don't keep state at same level as component responsible for displaying. 
     this way. only SearchBar has access to precise searchTerm at any one time, 
-    the App does not and cannot call real time methods on searchTerm. 
+    the App cannot call spotify in real time with searchTerm. Autocomplete. 
+    searches that change without clicking on search button is out.
     */
     const search=()=>{
         // search is to call on App's search function passed down with props.onSearch
         props.onSearch(searchTerm);
     }
 
-    // function to call props.onChange for input's value. 
+    const handleTermChange = (event) => {
+        // function to call props.onChange for input's value. 
+        // except state lives in searchBar atm, rather than App!
+        const term = event.target.value;
+        setSearchTerm(term);
+    }
+
 
     return (
         <div className="SearchBar">
-            <input placeholder="Enter A Song, Album, or Artist" />
+            <input placeholder="Enter A Song, Album, or Artist" onChange={handleTermChange} />
             <button className="SearchButton" onClick={search}>SEARCH</button>
         </div>
     )

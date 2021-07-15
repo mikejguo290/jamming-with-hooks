eb0bf02d9cd308925408933a2961014dc51b6ab0
mikejguo290 <mikejguo290@gmail.com>
2021-07-16 00:08:16 +0100

    add redirectURI to spotify app with url for localhost.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -4,7 +4,14 @@ const Spotify = {
     //create Spotify module as an object
     getAccessToken(){
         // return access_token if it exists. if not, extract it. 
-        // call this function after final URL is obtained. 
+        // call this function after final URL is obtained. this happens only after User logs in and authorizes app access 
+        // this is not the first step, which for the app is to request authorization from Spotify Accounts Service.
+        /* 
+        Step 80 of jamming tutorial is hard because it is five steps in one. 
+        Dealing with authorization but doesn't implement the first step, as you might expect. (and doesn't help you locate which step).
+        Uses Regex 
+        Leans heavily into a previously untaught API - Uses windows API three times 
+        */
 
         // check if access_token is already set, if so, return it 
         if(accessToken){
@@ -19,7 +26,7 @@ const Spotify = {
             // extract access_token parameter value
 
             const accessTokenRegex = '[#&]access_token=([^&]+).*';
-            accessToken = url.match(accessTokenRegex)[1];
+            accessToken = url.match(accessTokenRegex)[1]; // this affects global variable accessToken.
             // extract expires_in parameter value
             const expiresInRegex = '[#&]expires_in=([^&]+).*';
             const expiresIn = parseInt(url.match(expiresInRegex)[1]);
@@ -29,6 +36,9 @@ const Spotify = {
             
             // clean the url of its parameters
             window.history.pushState({}, null, '/'); // window.history.pushState creates a new browser history entry setting the state, title, and url. i.e. set url without the parameters.
+        }else{
+            //accessToken is empty AND the final URL doesn't contain accessToken as a parameter.
+
         }
 
 
@@ -41,7 +51,7 @@ const Spotify = {
         // 2. get access token from the final url if user grants permission. 
         //https://example.com/callback#access_token=NwAExz...
         const clientID='0a66fe9778db4fa8923d80227cc6f6d9';
-        const redirectURI='something';
+        const redirectURI='http://localhost:3000/';
         const scope='playlist-read-private%20playlist-modify-private'
 
     }

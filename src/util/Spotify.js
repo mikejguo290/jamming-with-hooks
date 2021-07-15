6ebc71a1dd43777efb6a8045c406d39e5353be5f
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 23:44:04 +0100

    if accessToken doesn't have value. try and get it from final URL. Use window api to set token expiration and pushState clean url.

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -11,6 +11,25 @@ const Spotify = {
             return accessToken;
         };
         
+        // if access token isn't set, check if the final URL for the access token.
+        if(window.location.href.indexOf('access_token')!==-1 && window.location.href.indexOf('expires_in')!==-1){ // newer method String.includes(substring), 
+            // if access token is in the final url, extract access_token, set expiry time for access token.
+            
+            const url = Window.location.href;
+            // extract access_token parameter value
+
+            const accessTokenRegex = '[#&]access_token=([^&]+).*';
+            accessToken = url.match(accessTokenRegex)[1];
+            // extract expires_in parameter value
+            const expiresInRegex = '[#&]expires_in=([^&]+).*';
+            const expiresIn = parseInt(url.match(expiresInRegex)[1]);
+            
+            // set access token to expire at a time
+            window.setTimeout(()=> accessToken='', expiresIn*1000 ); // window.setTimeout executes a function after a delay, note setTimeout accepts delay arg in miliseconds.
+            
+            // clean the url of its parameters
+            window.history.pushState({}, null, '/'); // window.history.pushState creates a new browser history entry setting the state, title, and url. i.e. set url without the parameters.
+        }
 
 
         // using implicit grant flow

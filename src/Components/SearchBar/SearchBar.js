396e04a635b05047104f308e621d0ce94e359efc
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 15:25:19 +0100

    create searchTerm state, wrapper function search and use search as button's onClick event handler.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,10 +1,25 @@
 import './SearchBar.css';
+import { useState } from 'react';
 
-function searchBar(){
+function searchBar(props){
+    const [searchTerm, setSearchTerm] = useState('');
+    /* where to keep this search term state? this is similar to playlistName 
+    and yet it isn't tracked at App level? contravenes the 4th principle of thinking in react.  
+    don't keep state at same level as component responsible for displaying. 
+    this way. only SearchBar has access to precise searchTerm at any one time, 
+    the App does not and cannot call real time methods on searchTerm. 
+    */
+    const search=()=>{
+        // search is to call on App's search function passed down with props.onSearch
+        props.onSearch(searchTerm);
+    }
+
+    // function to call props.onChange for input's value. 
+
     return (
         <div className="SearchBar">
             <input placeholder="Enter A Song, Album, or Artist" />
-            <button className="SearchButton">SEARCH</button>
+            <button className="SearchButton" onClick={search}>SEARCH</button>
         </div>
     )
 }

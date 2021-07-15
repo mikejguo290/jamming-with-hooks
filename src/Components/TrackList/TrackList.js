c927b1618dff9c4106ecf95b5c96b53a7d13201a
mikejguo290 <mikejguo290@gmail.com>
2021-07-15 12:19:05 +0100

    pass down onRemove as props from TrackList to Track.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,7 +5,7 @@ function TrackList(props) {
     return (
         <div className="TrackList">
             {/* <!-- You will add a map method that renders a set of Track components  --> */}
-            {props.tracks.map(track => <Track key={track.id} track={track} onAdd={props.onAdd} isRemoval={props.isRemoval} />)}
+            {props.tracks.map(track => <Track key={track.id} track={track} onAdd={props.onAdd} onRemove={props.onRemove} isRemoval={props.isRemoval} />)}
         </div>
     );
 };
